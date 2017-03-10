0256b4080ff05433fbbcc25c2013f2e328f46a91
mahe <mahesh9415@gmail.com>
2017-03-10 15:23:40 +0530

    firstdeploy

--- public/index.js
+++ public/index.js
@@ -10,19 +10,10 @@ socket.on('disconnect',function(){
 	console.log("disconnected!!!");
 });
 
-socket.emit('createMsg',{
-	text : "hello this is a msg from browser"
-});
-
-
 socket.on('newMsg',function(msg)
 {
 	console.log(msg);
 });
-
-socket.on('send',function(obj){
-	console.log(obj)
-})
 
 
 
